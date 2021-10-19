f7872c1164462827ecef5edf4ffcec9ae050f044
Venkata Bhaskaram Varanasi <venkatabhaskaram.varanasi@verizonmedia.com>
2021-10-19 09:44:41 -0700

    fix linting

--- zpeclient.go
+++ zpeclient.go
@@ -415,7 +415,7 @@ func (c *Cache) updateCacheStatus(timestamp string) error {
 
 // SetCacheEnabledStatus - sets cacheEnabled value
 // when cacheEnabled is set to true, cache will be used in the request flow.
-func (c *Cache) SetCacheEnabledStatus(status  bool) {
+func (c *Cache) SetCacheEnabledStatus(status bool) {
 	c.log.Printf("set cache enabled status to %t \n", status)
 	c.cmLock.Lock()
 	defer c.cmLock.Unlock()
